c821e4e2029e56f3a4017cd03880bf7935e57fed
João Prates <55405503+joaoprates@users.noreply.github.com>
2024-03-01 19:38:16 -0300

    Add files via upload

---
+++ data-structure-and-algorithms/src/main/java/dsa/vector/teste/Class05.java
@@ -0,0 +1,18 @@
+package dsa.vector.teste;
+
+import dsa.vector.Vector;
+public class Class05 {
+    public static void main(String[] args) {
+
+        Vector vector = new Vector(10);
+
+        vector.add("element 1");
+        vector.add("element 2");
+        vector.add("element 3");
+
+        System.out.println(vector.length());
+
+        System.out.println(vector.toString());
+    }
+
+}
